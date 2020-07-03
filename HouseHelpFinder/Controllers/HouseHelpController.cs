232d6ead76a1e8552d4d26acf5f98eac008533c3
Haron Kipkorir <developerharon@users.noreply.github.com>
2020-07-04 00:09:59 +0300

    Created the change password functionality

--- HouseHelpFinder/Controllers/HouseHelpController.cs
+++ HouseHelpFinder/Controllers/HouseHelpController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,11 +15,15 @@ namespace HouseHelpFinder.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IUserValidator<ApplicationUser> _userValidator;
+        private readonly IPasswordValidator<ApplicationUser> _passwordValidator;
+        private readonly IPasswordHasher<ApplicationUser> _passwordHasher;
 
-        public HouseHelpController(UserManager<ApplicationUser> userManager, IUserValidator<ApplicationUser> userValidator)
+        public HouseHelpController(UserManager<ApplicationUser> userManager, IUserValidator<ApplicationUser> userValidator, IPasswordValidator<ApplicationUser> passwordValidator, IPasswordHasher<ApplicationUser> passwordHasher)
         {
             _userManager = userManager;
             _userValidator = userValidator;
+            _passwordValidator = passwordValidator;
+            _passwordHasher = passwordHasher;
         }
 
         public ViewResult Index()
@@ -137,6 +140,54 @@ namespace HouseHelpFinder.Controllers
             {
                 ModelState.AddModelError("", "User Not Found");
             }
+            return View(model);
+        }
+
+        public IActionResult ChangePassword() => View();
+
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            // Checks that all the fields are filled before being submitted
+            if (ModelState.IsValid)
+            {
+                // Checks that the user has entered the correct password
+                if (model.NewPassword != model.ConfirmNewPassword)
+                    ModelState.AddModelError("", "Passwords do not match");
+
+                // Get's the user object from the database
+                ApplicationUser user = await _userManager.FindByIdAsync(model.HouseHelpId);
+
+                // If the user is not found render the default view to show the error.
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "User Not Found");
+                    return View(model);
+                }
+
+                // Checks the user has entered the right current password
+                if (!await _userManager.CheckPasswordAsync(user, model.CurrentPassword))
+                    ModelState.AddModelError("", "Wrong current password! Change not authorized");
+
+                // Validates that the new password is strong.
+                IdentityResult validateNewPassword = await _passwordValidator.ValidateAsync(_userManager, user, model.NewPassword);
+
+                // If the new password is strong, hash it and save it to the user object else add errors from the operation above
+                if (validateNewPassword.Succeeded)
+                    user.PasswordHash = _passwordHasher.HashPassword(user, model.NewPassword);
+                else
+                    AddErrorsFromResult(validateNewPassword);
+
+                // Update the user object in the database to reflect the new password.
+                IdentityResult result = await _userManager.UpdateAsync(user);
+
+                // If the operation is successful redirect the user to the profile page else add errors to the model state
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    AddErrorsFromResult(result);
+            }
+
+            // Render the default view to show the errors
             return View(model);
         }
 
---
+++ HouseHelpFinder/Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseHelpFinder.Models.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        public string HouseHelpId { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string CurrentPassword { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string NewPassword { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
